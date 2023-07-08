ba3320044e251bae6579fab5ca5314940c69ac53
DmitriyZit <adima74@gmail.com>
2023-07-08 14:13:57 +0300

    Сохранить

--- Program.cs
+++ Program.cs
@@ -12,7 +12,13 @@
 
       // 2. Ввести количество строк
       Console.Write("колличество строк = ");
+      
       count = Int32.Parse(Console.ReadLine());
+      if (count == 0)
+      {
+        Console.WriteLine("Введите число строк:");
+      }
+      
 
       // 3. Выделить память для массива из count строк
       AS = new string[count];
@@ -24,9 +30,10 @@
       {
         Console.Write("AS[{0}] = ", i);
         AS[i] = Console.ReadLine();
+        Console.WriteLine(AS[i].Length);
       }
 
-      // 5. Сортировка методом вставки
+      /* 5. Сортировка методом вставки
       for (int i = 0; i < AS.Length - 1; i++)
         for (int j = i; j >= 0; j--)
           if (String.Compare(AS[j], AS[j + 1]) > 0) // функция Compare()
@@ -36,13 +43,23 @@
             AS[j] = AS[j + 1];
             AS[j + 1] = s;
           }
-
+        */
       // 6. Вывести массив AS
+      for ( int i = 0; i < AS.Length - 1; i++ )
+      {
+        if  (int j=0; AS[j].Length <= 3; j++)
+        {
+            Console.WriteLine();
+            Console.WriteLine("все что меньше 3 :" , AS[j]);
+        }
+      }
+
       Console.WriteLine();
       Console.WriteLine("Отсортированный массив:");
       for (int i = 0; i < AS.Length; i++)
+      {
         Console.WriteLine("AS[{0}] = {1}", i, AS[i]);
-
+      }
       Console.ReadKey();
     }
   }
